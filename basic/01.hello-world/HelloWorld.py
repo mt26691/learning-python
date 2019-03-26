a15d2106c341604be73f7913683c54c37a865847
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-03-26 18:29:18 +0700

    hello world

---
+++ basic/01.hello-world/HelloWorld.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+#This is a comment
+if 5 > 2:
+    print("5 is greater than 2")
+    print("still in the if")
+    print("""This is a multiple
+    line of string""")
+else:
+    print("in the else")
