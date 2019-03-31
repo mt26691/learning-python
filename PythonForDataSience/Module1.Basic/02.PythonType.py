29bb9a5235f7bd5b6ac646928fddcbae3a7dc058
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-03-31 16:01:38 +0700

    basic

---
+++ PythonForDataSience/Module1.Basic/02.PythonType.py
@@ -0,0 +1,7 @@
+numberType = 11
+floatType = 21.3
+stringType = "test"
+print(type(numberType))
+print(type(floatType))
+print(type(stringType))
+print(type(True))
---
+++ PythonForDataSience/Module1.Basic/03.StringOperation.py
@@ -0,0 +1,4 @@
+Numbers="0123456"
+
+print(Numbers[::2])
+print("0123456".find("1"))
