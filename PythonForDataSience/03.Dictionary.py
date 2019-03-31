bd959b0a6c90daca17b789add81c9cb079a0e47e
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-03-31 16:48:59 +0700

    diction nary

--- PythonForDataSience/03.Dictionary.py
+++ PythonForDataSience/03.Dictionary.py
@@ -4,3 +4,6 @@
 sampleDictionary = {"name": "Nguyen Manh Tung", "age": 28}
 print(sampleDictionary)
 print(sampleDictionary["name"])
+soundtrack_dic = {"The Bodyguard":"1992", "Saturday Night Fever":"1977"}
+print(soundtrack_dic.keys())
+print(soundtrack_dic.values())
