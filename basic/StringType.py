092f5cc913ffc841e706e7a15f1911d863203f04
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-03-31 15:17:56 +0700

    casting, var

---
+++ basic/StringType.py
@@ -0,0 +1,5 @@
+x = 'String can be single quotation mark like JS'
+y = "string can be doulbe quotation"
+#Like many other popular programming languages, strings in Python are arrays of bytes representing unicode characters. 
+print(x)
+print(y)
